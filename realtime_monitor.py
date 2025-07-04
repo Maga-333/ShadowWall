748dc817c228c3f6de55155b27f34739d04dd79a
Maga-333 <etho.onnu333@gmail.com>
2025-07-04 21:40:44 +0530

    Update realtime_monitor.py

--- realtime_monitor.py
+++ realtime_monitor.py
@@ -3,35 +3,34 @@ from urllib.parse import urlparse
 import subprocess
 
 def get_ip_domain(url):
-try:
-parsed = urlparse(url)
-domain = parsed.netloc
-ip = socket.gethostbyname(domain)
-return domain, ip
-except:
-return None, None
+    try:
+        parsed = urlparse(url)
+        domain = parsed.netloc
+        ip = socket.gethostbyname(domain)
+        return domain, ip
+    except:
+        return None, None
 
 def check_redirection(url):
-try:
-r = requests.get(url, timeout=3, allow_redirects=True)
-hops = [resp.url for resp in r.history] + [r.url]
-return hops
-except:
-return ["[Error] Redirection Failed"]
+    try:
+        r = requests.get(url, timeout=3, allow_redirects=True)
+        hops = [resp.url for resp in r.history] + [r.url]
+        return hops
+    except:
+        return ["[Error] Redirection Failed"]
 
 def is_fake_domain(domain):
-with open("data/safe_domains.txt") as f:
-safe = f.read().splitlines()
-return domain not in safe
+    with open("data/safe_domains.txt") as f:
+        safe = f.read().splitlines()
+    return domain not in safe
 
 def analyze_link(url):
-domain, ip = get_ip_domain(url)
-redirs = check_redirection(url)
-fake = is_fake_domain(domain)
-return {
-"Domain": domain,
-"IP": ip,
-"Redirections": redirs,
-"Fake": fake
-}
-
+    domain, ip = get_ip_domain(url)
+    redirs = check_redirection(url)
+    fake = is_fake_domain(domain)
+    return {
+        "Domain": domain,
+        "IP": ip,
+        "Redirections": redirs,
+        "Fake": fake
+    }
