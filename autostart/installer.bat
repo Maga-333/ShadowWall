239ddbf73124250b5135d989f2e17e5dc7b73f96
Maga-333 <etho.onnu333@gmail.com>
2025-07-04 20:46:36 +0530

    Update installer.bat

--- autostart/installer.bat
+++ autostart/installer.bat
@@ -1,3 +1,4 @@
 @echo off
-copy shadowwall_daemon.py "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-start shadowwall_daemon.py
+echo Adding ShadowWall to system startup...
+copy "%~dp0\..\shadowwall_daemon.py" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\shadowwall_daemon.py"
+echo Done! ShadowWall will run on system start.
