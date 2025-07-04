06106da2679330af7bd6e3b6638afb02333fae3d
Maga-333 <etho.onnu333@gmail.com>
2025-07-04 20:45:21 +0530

    Update url_analyzer.py

--- utils/url_analyzer.py
+++ utils/url_analyzer.py
@@ -1,1 +1,15 @@
+import subprocess
+import socket
 
+def get_ip(domain):
+    try:
+        return socket.gethostbyname(domain)
+    except:
+        return "N/A"
+
+def traceroute(domain):
+    try:
+        result = subprocess.check_output(["tracert", domain], shell=True)
+        return result.decode()
+    except:
+        return "Failed traceroute"
