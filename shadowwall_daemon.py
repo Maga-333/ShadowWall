0b3c89ce317026aca71824410931ed798a5e2292
Maga-333 <etho.onnu333@gmail.com>
2025-07-04 20:40:38 +0530

    Update shadowwall_daemon.py

--- shadowwall_daemon.py
+++ shadowwall_daemon.py
@@ -1,1 +1,16 @@
+from realtime_monitor import analyze_link
+import time, pyperclip
 
+scanned_links = set()
+
+def monitor_clipboard():
+    while True:
+        url = pyperclip.paste()
+        if url.startswith("http") and url not in scanned_links:
+            result = analyze_link(url)
+            print("[ShadowWall Scan]")
+            print(result)
+            scanned_links.add(url)
+        time.sleep(3)
+
+monitor_clipboard()
