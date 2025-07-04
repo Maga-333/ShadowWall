d63bc78d17237c6bdd5f10648e7c419407bcf7d5
Maga-333 <etho.onnu333@gmail.com>
2025-07-04 21:30:47 +0530

    Update url_analyzer.py

--- utils/url_analyzer.py
+++ utils/url_analyzer.py
@@ -1,5 +1,5 @@
+import subprocess
 import socket
-from ipwhois import IPWhois
 
 def get_ip(domain):
     try:
@@ -7,27 +7,9 @@ def get_ip(domain):
     except:
         return "N/A"
 
-def get_ip_info(ip):
-    try:
-        obj = IPWhois(ip)
-        data = obj.lookup_rdap()
-        return {
-            "Country": data.get("network", {}).get("country", "Unknown"),
-            "Org": data.get("network", {}).get("name", "Unknown"),
-            "ASN": data.get("asn", "Unknown")
-        }
-    except:
-        return {
-            "Country": "Unknown",
-            "Org": "Unknown",
-            "ASN": "Unknown"
-        }
-
-# Optional: traceroute
 def traceroute(domain):
     try:
-        import subprocess
         result = subprocess.check_output(["tracert", domain], shell=True)
         return result.decode()
     except:
-        return "Traceroute Failed"
+        return "Failed traceroute"
