d9c284f8ea2d64ce20e48e73eb540fb29498438e
Maga-333 <etho.onnu333@gmail.com>
2025-07-04 20:36:59 +0530

    Update injector.js

--- injector/injector.js
+++ injector/injector.js
@@ -1,1 +1,7 @@
-
+const forms = document.querySelectorAll("form");
+forms.forEach(form => {
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("🚨 ShadowWall blocked this suspicious login page!");
+  });
+});
